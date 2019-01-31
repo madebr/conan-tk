8775315a7db10f4e70b89785dba2ee56bace0d79
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-01-31 03:02:17 +0100

    Switch to plain conan_package_tools

--- build.py
+++ build.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_default
+from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-
-    builder = build_template_default.get_builder()
-
+    builder = ConanMultiPackager()
+    builder.add_common_builds()
     builder.run()
